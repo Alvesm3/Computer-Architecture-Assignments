6f238d6e8799deca6ed1b6446f23e2d41f756f01
Matthew Alves <45500132+Alvesm3@users.noreply.github.com>
2019-07-25 20:21:18 -0400

    Updated Assignment5.py

--- assignment5.py
+++ assignment5.py
@@ -2,16 +2,20 @@
 with open("Assignment5_input.txt", 'r') as instructionsobj:
     for line in instructionsobj:
         instruction_memory = list(instructionsobj) #store each line into a list called instruction_memory -> this will allow for easy indexing of each instruction
-        #print(a)
+        #print(instruction_memory)
 
 #Read the file with all the registers       
 with open("registers.txt", 'r+') as registersobj:
-    for lines in registersobj:    
-        registers = registersobj = list(registersobj)
-        print(registers[0])
+    for lines in registersobj:
+        registers = list(registersobj)
+register_list = []
+for i in registers:
+    register_list.append(int(i.strip()))
+print(register_list)
+print(register_list[0])
 
+data_memory = [10, 13]
 
-        
 PC = 0 #initialize pogram counter
 
 #print the instruction_memory - test
@@ -30,18 +34,109 @@ for PC in range(8):
     print(instruction_memory[PC])
     if instruction_memory[PC][0] == 'A':
         if instruction_memory[PC][3] == 'I':
-            #print("ADDI")
+            print("ADDI")
+            w = instruction_memory[PC][6]
+            Rd = w + instruction_memory[PC][7]
+            Rd = int(Rd)
+            print(type(Rd))
             x = instruction_memory[PC][11]
             Rn = x + instruction_memory[PC][12]
-            #print(Rn)
+            Rn = int(Rn)
+            print(type(Rn))
             y = instruction_memory[PC][16]
-            Rd = y + instruction_memory[PC][17]
-            #print(Rd)
-            print("ADDI", Rn, Rd)
-            
+            Rm = y + instruction_memory[PC][17]
+            Rm = int(Rm)
+            print(type(Rm))
+            print("ADDI", Rn, Rm)
+            wtor = register_list[int(Rn)-1] + int(Rm)
+            print(wtor)
+            register_list.pop(Rd-1)
+            register_list.insert(Rd-1, wtor)
+            print(register_list)
+            PC = PC + 1
         elif instruction_memory[PC][3] == ' ':
             print("ADD")
+            #print(instruction_memory[PC-1])
+            w = instruction_memory[PC][6]
+            Rd = w + instruction_memory[PC][7]
+            Rd = int(Rd)
+            print(Rd)
+            x = instruction_memory[PC][11]
+            Rn = x + instruction_memory[PC][12]
+            Rn = int(Rn)
+            print(Rn)
+            y = instruction_memory[PC][16]
+            Rm = y + instruction_memory[PC][17]
+            Rm = int(Rm)
+            print(Rm)
+            wtor = register_list[Rn-1] + register_list[Rm-1]
+            print(wtor)
+            register_list.pop(Rd-1)
+            register_list.insert(Rd-1, wtor)
+            print(register_list)
+            PC = PC + 1
     if instruction_memory[PC][0] == 'S':
         if instruction_memory[PC][3] == ' ':
             print("SUB")
+            w = instruction_memory[PC][6]
+            Rd = w + instruction_memory[PC][7]
+            Rd = int(Rd)
+            print(Rd)
+            x = instruction_memory[PC][11]
+            Rn = x + instruction_memory[PC][12]
+            Rn = int(Rn)
+            print(Rn)
+            y = instruction_memory[PC][16]
+            Rm = y + instruction_memory[PC][17]
+            Rm = int(Rm)
+            print(Rm)
+            wtor = register_list[Rn-1] - register_list[Rm-1]
+            print(wtor)
+            register_list.pop(Rd-1)
+            register_list.insert(Rd-1, wtor)
+            print(register_list)
+            PC = PC + 1 
+        elif instruction_memory[PC][3] == 'I'
+            print("SUBI")
+            w = instruction_memory[PC][6]
+            Rd = w + instruction_memory[PC][7]
+            Rd = int(Rd)
+            print(Rd)
+            x = instruction_memory[PC][11]
+            Rn = x + instruction_memory[PC][12]
+            Rn = int(Rn)
+            print(Rn)
+            y = instruction_memory[PC][16]
+            Rm = y + instruction_memory[PC][17]
+            Rm = int(Rm)
+            print(Rm)
+            wtor = register_list[Rn-1] - register_list[Rm-1]
+            print(wtor)
+            register_list.pop(Rd-1)
+            register_list.insert(Rd-1, wtor)
+            print(register_list)
+            PC = PC + 1
+    if instruction_memory[PC][0] == 'L':
+        if instruction_memory[PC][3] == 'R':
+            w = instruction_memory[PC][6]
+            Rd = w + instruction_memory[PC][7]
+            Rd = int(Rd)
+            print(type(Rd))
+            x = instruction_memory[PC][11]
+            Rn = x + instruction_memory[PC][12]
+            Rn = int(Rn)
+            print(type(Rn))
+            y = instruction_memory[PC][16]
+            Rm = y + instruction_memory[PC][17]
+            Rm = int(Rm)
+            print(type(Rm))
+            print("ADDI", Rn, Rm)
+            wtor = register_list[int(Rn)-1] + int(Rm)
+            print(wtor)
+            register_list.pop(Rd-1)
+            register_list.insert(Rd-1, wtor)
+            print(register_list)  
+
+        
+
         
