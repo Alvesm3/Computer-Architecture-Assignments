0a0d37b77f692ee14c859f4e9d1df00524cc1755
Matthew Alves <45500132+Alvesm3@users.noreply.github.com>
2019-07-25 20:30:55 -0400

    update assignment5.py

--- assignment5.py
+++ assignment5.py
@@ -30,7 +30,7 @@ print(instruction_memory[0][11],instruction_memory[0][12] )
 #print(instruction_memory[1][0], instruction_memory[1][3])
 
  
-for PC in range(8):
+for PC in range(11):
     print(instruction_memory[PC])
     if instruction_memory[PC][0] == 'A':
         if instruction_memory[PC][3] == 'I':
@@ -95,8 +95,29 @@ for PC in range(8):
             register_list.pop(Rd-1)
             register_list.insert(Rd-1, wtor)
             print(register_list)
-            PC = PC + 1 
-        elif instruction_memory[PC][3] == 'I'
+            PC = PC + 1
+        elif instruction_memory[PC][3] == 'R':
+            print("STUR")
+            w = instruction_memory[PC][6]
+            Rd = w + instruction_memory[PC][7]
+            Rd = int(Rd)
+            print(type(Rd))
+            x = instruction_memory[PC][11]
+            Rn = x + instruction_memory[PC][12]
+            Rn = int(Rn)
+            print(type(Rn))
+            y = instruction_memory[PC][16]
+            Rm = y + instruction_memory[PC][17]
+            Rm = int(Rm)
+            print(type(Rm))
+            print("ADDI", Rn, Rm)
+            wtor = register_list[int(Rn)-1] + int(Rm)
+            print(wtor)
+            wtor = data_memory[wtor]
+            register_list.pop(Rd-1)
+            register_list.insert(Rd-1, wtor)
+            print(register_list)  
+        elif instruction_memory[PC][3] == 'I':
             print("SUBI")
             w = instruction_memory[PC][6]
             Rd = w + instruction_memory[PC][7]
@@ -118,6 +139,7 @@ for PC in range(8):
             PC = PC + 1
     if instruction_memory[PC][0] == 'L':
         if instruction_memory[PC][3] == 'R':
+            print("LDUR")
             w = instruction_memory[PC][6]
             Rd = w + instruction_memory[PC][7]
             Rd = int(Rd)
@@ -133,9 +155,11 @@ for PC in range(8):
             print("ADDI", Rn, Rm)
             wtor = register_list[int(Rn)-1] + int(Rm)
             print(wtor)
+            wtor = data_memory[wtor]
             register_list.pop(Rd-1)
             register_list.insert(Rd-1, wtor)
-            print(register_list)  
+            print(register_list)
+            PC = PC + 1
 
         
 
