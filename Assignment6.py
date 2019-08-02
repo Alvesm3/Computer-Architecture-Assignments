5d4c8b998f649de07aac9deee3f65f95fb2bbfe6
Matthew Alves <45500132+Alvesm3@users.noreply.github.com>
2019-08-02 16:40:46 -0400

    Updated

--- Assignment6.py
+++ Assignment6.py
@@ -23,53 +23,137 @@ ADDI = 0
 ADD = 1 
 SUBI = 2 
 SUB = 3 
-LDUR = 4 
-STUR = 5 
+STUR = 4 
+LDUR = 5 
 CBZ = 6 
 B = 7
 '''
 def instructiondecode(enter):
     decode = []
     if enter[0] == 'A':
-        if enter[PC][3] == 'I':
+        if enter[3] == 'I':
             decode[0] = 0
-            w = enter[PC][6]
-            Rd = w + enter[PC][7]
+            w = enter[6]
+            Rd = w + enter[7]
             Rd = int(Rd)
             decode[1] = Rd
-            x = enter[PC][11]
-            Rn = x + enter[PC][12]
+            x = enter[11]
+            Rn = x + enter[12]
             Rn = int(Rn)
             decode[2] = Rn
-            y = enter[PC][16]
-            z = y + enter[PC][17]
-            imm = z + enter[PC][18]
+            y = enter[16]
+            z = y + enter[17]
+            imm = z + enter[18]
             imm = int(imm)
             decode[3] = imm
             decode[4] = 0
             decode[5] = 0
-        elif enter[PC][3] == ' ':
+        elif enter[3] == ' ':
             decode[0] = 1
-            w = enter[PC][6]
-            Rd = w + enter[PC][7]
+            w = enter[6]
+            Rd = w + enter[7]
             Rd = int(Rd)
             decode[1] = Rd
-#------------------------------------------------------------#            
-            x = enter[PC][11]
-            Rn = x + enter[PC][12]
+            x = enter[11]
+            Rn = x + enter[12]
             Rn = int(Rn)
             decode[2] = Rn
-#------------------------------------------------------------#            
-            y = enter[PC][16]
-            Rm = y + enter[PC][17]
+            y = enter[16]
+            Rm = y + enter[17]
             Rm = int(Rm)
             decode[5] = Rm
             decode[3] = 0
             decode[4] = 0
-    if enter[PC][0] == 'S':
-        if enter[PC][3] == 'I':
-            
-            
+    if enter[0] == 'S':
+        if enter[3] == 'I':
+            decode[0] = 2
+            w = enter[6]
+            Rd = w + enter[7]
+            Rd = int(Rd)
+            decode[1] = Rd
+            x = enter[11]
+            Rn = x + enter[12]
+            Rn = int(Rn)
+            decode[2] = Rn
+            y = enter[16]
+            z = y + enter[17]
+            imm = z + enter[18]
+            imm = int(imm)
+            decode[3] = imm
+            decode[4] = 0
+            decode[5] = 0
+        elif enter[3] == ' ':
+            decode[0] = 3
+            w = enter[6]
+            Rd = w + enter[7]
+            Rd = int(Rd)
+            decode[1] = Rd
+            x = enter[11]
+            Rn = x + enter[12]
+            Rn = int(Rn)
+            decode[2] = Rn
+            y = enter[16]
+            Rm = y + enter[17]
+            Rm = int(Rm)
+            decode[5] = Rm
+            decode[3] = 0
+            decode[4] = 0
+        elif enter[3] == 'R':
+            decode[0] = 4
+            w = enter[6]
+            Rt = w + enter[7]
+            Rt = int(Rt)
+            decode[4] = Rt
+#------------------------------------------------------------#            
+            x = enter[11]
+            Rn = x + enter[12]
+            Rn = int(Rn)
+            decode[2] = Rn
+#------------------------------------------------------------#            
+            y = enter[16]
+            address = y + enter[17]
+            address = int(address) 
+            decode[3] = address
+            decode[1] = 0
+            decode[5] = 0
+    if enter[0] == 'L':
+        decode[0] = 5
+        w = enter[6]
+        Rt = w + enter[7]
+        Rt = int(Rt)
+        decode[4] = Rt
+#------------------------------------------------------------#            
+        x = enter[11]
+        Rn = x + enter[12]
+        Rn = int(Rn)
+        decode[2] = Rn
+#------------------------------------------------------------#            
+        y = enter[16]
+        address = y + enter[17]
+        address = int(address) 
+        decode[3] = address
+        decode[1] = 0
+        decode[5] = 0  
+    if enter[0] == 'C':
+        decode[0] = 6
+        w = enter[6] 
+        Rt = w + enter[7]
+        Rt = int(Rt)
+        decode[4] = Rt
+        decode[1] = 0
+        decode[2] = 0
+        decode[3] = 0
+        decode[5] = 0
+    if enter[0] == 'B':
+        decode[0] = 7
+        w = enter[6]
+        num = w + enter[7]
+        num = int(num)-1
+        decode[3] = num
+        decode[1] = 0
+        decode[2] = 0
+        decode[4] = 0
+        decode[5] = 0
 
 def execute():
     pass
